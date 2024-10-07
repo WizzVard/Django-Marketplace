380b9ddb6d75999212eba99be8622b15093ab269
Vadim Kustov <wizzvard22@gmail.com>
2024-10-08 00:14:18 +0300

    Add functionality for the project

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'item'
+    'item',
+    'dashboard',
+    'conversation'
 ]
 
 MIDDLEWARE = [

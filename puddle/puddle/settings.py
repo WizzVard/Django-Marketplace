c95d0ea89d673ef3fd958bcdf3f1069441183ad8
Vadim Kustov <wizzvard22@gmail.com>
2024-10-08 20:55:50 +0300

    add inbox page and fix a bug

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conversation',
     'core',
     'item',
     'dashboard',
-    'conversation'
 ]
 
 MIDDLEWARE = [
